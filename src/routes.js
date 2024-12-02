4feb7a9388df84f5e7af9f551bd99666f0fde146
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-12-02 19:29:58 -0300

    Configurando os componentes cards e rodape

--- src/routes.js
+++ src/routes.js
@@ -2,18 +2,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
 console.log(window.location)
 
 function AppRoutes() {
   return(
     <BrowserRouter>
       <Menu />
+      
       <Routes>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
         <Route path="*" element={<div>Pagina nao encontrada</div>} />
-
       </Routes>
+
+      <Rodape />
+    
     </BrowserRouter>
   )
 }
