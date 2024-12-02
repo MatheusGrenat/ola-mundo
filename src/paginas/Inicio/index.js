920e8415a28f7734888027bd2923f740c6c4c25c
MatheusGrenat <matheusgrenat@yahoo.com.br>
2024-12-02 19:41:21 -0300

    Refatorando a rota inicio e sobremim e criando um componente para reutilizar o codigo com Outlet

--- src/paginas/Inicio/index.js
+++ src/paginas/Inicio/index.js
@@ -1,23 +1,16 @@
-import Banner from "componentes/Banner";
 import styles from "paginas/Inicio/Inicio.module.css"
-
 import posts from 'json/posts.json';
 import Post from "componentes/Post";
 
 export default function Inicio(){
     return(
-        <main>
-            <Banner />
-            
-            <ul className={styles.posts}>
-                {posts.map((post) => (
-                    <li key={post.id}>
-                        <Post post={post} />
-                    </li>
-                ))}
-            </ul>
+        <ul className={styles.posts}>
+            {posts.map((post) => (
+                <li key={post.id}>
+                    <Post post={post} />
+                </li>
+            ))}
+        </ul>
  
- 
-        </main>
     )
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import Inicio from "./paginas/Inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
+import PaginaPadrao from "componentes/PaginaPadrao";
 console.log(window.location)
 
 function AppRoutes() {
@@ -11,6 +12,10 @@ function AppRoutes() {
       <Menu />
       
       <Routes>
+        <Route path="/" element={<PaginaPadrao />}>
+          <Route path="/" element={<Inicio />} />
+          <Route path="sobremim" element={<SobreMim />} />
+        </Route>
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
         <Route path="*" element={<div>Pagina nao encontrada</div>} />
