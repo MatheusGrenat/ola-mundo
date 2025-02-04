49efee1b70038ae511c25d31b9d81898ae497930
MatheusGrenat <matheusgrenat@yahoo.com.br>
2025-02-04 20:10:00 -0300

    Criando a pagina de erro 404 e estilizando as paginas dos posts

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
 import Post from "paginas/Post";
+import NaoEncontrada from "paginas/NaoEncontrada";
 console.log(window.location)
 
 function AppRoutes() {
@@ -20,7 +21,7 @@ function AppRoutes() {
           <Route path="posts/:id" element={<Post />} />
         </Route>
         
-        <Route path="*" element={<div>Pagina nao encontrada</div>} />
+        <Route path="*" element={<NaoEncontrada />} />
       </Routes>
 
       <Rodape />
